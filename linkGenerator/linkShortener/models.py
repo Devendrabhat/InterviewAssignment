c46095614954bfe2aa474c3f9459ffea9a827c91
Devendra Bhat <devendra.bhat2@gmail.com>
2018-03-25 01:27:06 +0530

    One link will get only one short link

--- linkGenerator/linkShortener/models.py
+++ linkGenerator/linkShortener/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class LinkModel(models.Model):
     link = models.CharField(max_length=500)
     shortLink = models.CharField(max_length=30,unique=True)
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.link)
--- linkGenerator/linkShortener/views.py
+++ linkGenerator/linkShortener/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
-from.models import LinkModel
+from .models import LinkModel
 from .forms import LinkForm
 from .linkGenerator import Generator
 # Create your views here.
@@ -11,23 +11,40 @@ class HomeView(TemplateView):
     template_name = 'home.html'
     output = 'output.html'
     gen = Generator()
+    linksAndShortlinks = {}
 
     def get(self, request, *args, **kwargs):
         form = LinkForm()
-        return render(request,self.template_name,{'form':form})
+        links = LinkModel.objects.all().order_by('-date')
+
+        allLinks = links[0]
+        # Extracting out all the stored links with short links
+        iterator = 0
+        while iterator < len(links):
+            self.linksAndShortlinks[allLinks.link] = allLinks.shortLink
+            if iterator != (len(links))-1:
+                allLinks = allLinks.get_next_by_date()
+            iterator+=1
+        print(self.linksAndShortlinks)
+        return render(request,self.template_name,{'form':form,'links':links})
 
     def post(self,request, *args, **kwargs):
         form = LinkForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['link']
+        links = LinkModel.objects.all().order_by('-date')
 
-        dbobj = LinkModel()
-        dbobj.link = text
-        self.no = self.gen.generateShortLink()
-        dbobj.shortLink = str(self.no)
-        dbobj.save()
+        if not text in self.linksAndShortlinks:
+            dbobj = LinkModel()
+            dbobj.link = text
+            self.no = self.gen.generateShortLink()
+            dbobj.shortLink = str(self.no)
+            dbobj.save()
+            context = {'text':text,'no':self.no}
+        else:
+            context = {'text':text,'no':self.linksAndShortlinks[text]}
 
-        return render(request,self.output,{'text':text,'no':self.no})
+        return render(request,self.output,context=context)
 
 
 # def home(request):
